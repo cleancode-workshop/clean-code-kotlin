21d19041887c466b7372c134d8d379bc364e788e
Harshad Nawathe <harshadn@thoughtworks.com>
2021-04-21 13:37:58 +0530

    Harshad - Add a sample test using kotest assertion library and junit5

---
+++ src/test/kotlin/com/myflix/biller/CustomerTest.kt
@@ -0,0 +1,13 @@
+package com.myflix.biller
+
+import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Test
+
+class CustomerTest {
+
+    @Test
+    fun test() {
+        1 + 1 shouldBe 2
+    }
+
+}
